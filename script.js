ffbe841e79c2646dd48ef2da3fe3e448cec6224f
sakshi sharma <sakshisharma57268@gmail.com>
2025-06-12 16:24:23 +0530

    Add website files

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function data()
+{
+var a = document.getElementById("nl").value;
+var b = document.getElementById("n2").value;
+var c  = document.getElementById("n3").value;
+
+if(a=="" || b==""|| c=="")
+{
+    alert("all fields are mendatory");
+}
+
+}
